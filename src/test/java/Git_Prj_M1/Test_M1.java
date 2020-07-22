d8679822374dd0a2b8adc82bbbc80e14b3bfe891
sunil791 <sunildhut87@gmail.com>
2020-07-22 13:32:45 +0530

    Commited

---
+++ src/test/java/Git_Prj_M1/Test_M1.java
@@ -0,0 +1,28 @@
+package Git_Prj_M1;
+
+
+public class Test_M1 {
+
+	public static void biCycletype() {
+
+		System.out.println("Usage type : COMMERCIAL USE");
+	}
+	
+	private  static void gearSpec() {
+		
+		System.out.println("Gear combinations are : 6 x 5");
+	}
+	
+	
+	public static void main(String[] args) {
+
+		System.out.println("Git - Maven Project 1");
+		
+		biCycletype();
+		gearSpec();
+		
+	}
+
+	
+	
+}
